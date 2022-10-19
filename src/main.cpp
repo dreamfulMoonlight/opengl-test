dc9af0ebd722665ee74780b5b31170ff0c9d1555
ning.mao <ning.mao@smartmore.com>
2022-10-19 20:59:58 +0800

    feat(proc): add shader res

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include"render.h"
+#include<fstream>
+#include<string>
+#include<sstream>
+#include <direct.h>
+#include<limits.h>
+
 
 #include"VertexBuffer.h"
 #include"IndexBuffer.h"
@@ -21,6 +27,41 @@ static unsigned int compileShader(const std::string&source, unsigned int type)
 
 }
 
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+static ShaderProgramSource ParasseShader(const std::string&file_path) {
+	enum class ShaderType
+	{
+		NONE=-1,VERTEX=0,FRAGMENT=1
+	};
+	std::ifstream stream(file_path);
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+
+	while (getline(stream,line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			ss[(int)type] << line << "\n";
+		}
+	}
+	return { ss[0].str(),ss[1].str() };
+}
 
 static unsigned int CreateShader(const std::string& vertexShader, const std::string &fragmentShader)
 {
@@ -116,7 +157,12 @@ int main(int argc, char **argv)
 		"}\n";
 
 	
-		unsigned int shader = CreateShader(vertexShader, fragmentShader);
+		//unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	char buff[250];
+	_getcwd(buff, 250);
+	std::string current_working_directory(buff);
+		ShaderProgramSource source = ParasseShader("D:/test/opengl-test/src/basic.shader");
+		unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 		int location = glGetUniformLocation(shader, "ourColor");
 		float r = 0.0f;
 		float increment = 0.05f;
